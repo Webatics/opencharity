dd6f5a6449dd39c3a9838ff1dd36805cdf077930
Webatics <femmor2u@yahoo.com>
2016-12-09 03:30:18 +0100

    Add files via upload

---
+++ page.tpl.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/favicon.png">
+    <title>Open Charity</title>
+    <link href="style.css" rel="stylesheet">
+
+    <!-- For the Social Icons -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+    
+    <!-- Header Start -->
+  	<header>
+  		<nav>
+        <div class="container nav-container">
+          <div class="row">
+            <div class="col span-1-of-2">
+              <img src="https://www.dropbox.com/s/1qkmy1zp5tm8wsk/logo.png?raw=1" alt="logo" class="logo">
+            </div>
+            <div class="col span-1-of-2">
+              <ul class="topnav main-nav" id="myTopnav">
+                <li class="spaced"><a href="#">About Open Charity</a></li>                      
+                <li class="spaced"><a href="#">The Blog</a></li>  
+                <li class="signin-btn"><a href="#" class="signIn">Join Us</a></li>
+              </ul>
+          </div>
+        </div>
+  		</nav>
+    </header>
+
+    <!--  Header End -->
+
+    
+      <!-- Hero Section -->
+      <section class="hero-section">
+        <div class="hero-text-box">
+          <h1 class="hero-text-big">Sharing ideas For Charities</h1>
+          <p class="hero-p1">Many charities’ goals are similar, as is the functionality we require, but <br> little shared working takes place.</p> <br>
+            <p class="hero-p2">By working together, driving shared areas of interest and influencing open source <br> developments we can bring efficiencies, improve the digital experience for our users, and <br> have great impact.</p> <br>
+            <p class="hero-p1">Together we can make a bigger diference.</p>
+        </div>
+      </section>
+       <!-- Hero Section End -->
+      
+      <!-- Next Event Section -->
+      <section class="next-event-section">
+        <div class="next-container container">
+          <div class="row next-event-row">
+            <div class="col span-2-of-3">
+              <p class="event-p1"><strong><span class="colored-event">Next Event:</span> June 23<sup>rd</sup> 2016  18:30 - 21:00</strong><br>
+              Cancer Research UK, Angel Building, 407 St John Street, London EC1V 4AD </p>
+            </div>
+            <div class="col span-1-of-3">
+              <a href="#" class="registerBtn">Register</a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Next Event Section End -->
+
+      <!-- Get Involve Section -->
+      <section class="involve-section show-nav">
+        <div class="container">
+          <div class="involve-box">
+            <div class="row">
+              <h2>Get Involved</h2>
+              <div class="col span-1-of-3">
+                <img src="https://www.dropbox.com/s/48eol0lbqg79tdr/meetup.png?raw=1" class="involve-img" alt="">
+                <h3>We do meetings</h3>
+                <p>We organise our meetings through the OpenCharity MeetUp group</p>
+                <a href="#" class="joinUs">Meetup Group</a>
+              </div>
+              <div class="col span-1-of-3 join-col">
+                <img src="https://www.dropbox.com/s/zzwlvau8xl4w0nv/slack.png?raw=1" class="involve-img2"  alt="">
+                <h3>We Collaborate</h3>
+                <p>OpenCharity have a slack group for daily collaboration</p>
+                <a href="#" class="joinUs">Slack Group</a>
+              </div>
+              <div class="col span-1-of-3 join-col">
+                <img src="https://www.dropbox.com/s/mzt2mnrot13y8w2/google-group.png?raw=1" class="involve-img" alt="">
+                <h3>We Share</h3>
+                <p>We have a Google Group set up to share tools and documents</p>
+                <a href="#" class="joinUs">Google Group</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Get Involve Section End -->
+
+
+      <!-- Mission Section -->
+      <section class="mission-section">
+        <div class="mission-box">
+          <div class="container">
+              <div class="row">
+                  <h2>Our Mission</h2>
+                  <p>Charities and Partners collaborating and sharing open solutions and ideas to create value in the digital space.</p>
+                  <p>If you are a charity or a supplier, we are ready to welcome you.</p>
+                  <div class="col span-1-of-3">
+                    <img src="https://www.dropbox.com/s/dbb640s8arwuxwu/icon-bulb.png?raw=1" class="" alt="">
+                    <div class="mission-card">
+                      <h3>We help charities</h3>
+                      <p>We organise our meetings through the OpenCharity MeetUp group</p>
+                    </div>
+                    
+                  </div>
+                  <div class="col span-1-of-3">
+                    <img src="https://www.dropbox.com/s/a2vnprow2a93iwt/icon-like.png?raw=1" class=""  alt="">
+                    <div class="mission-card">
+                      <h3>We bring together</h3>
+                      <p>OpenCharity have a slack group for daily collaboration</p>
+                    </div>
+                  </div>
+                  <div class="col span-1-of-3">
+                    <img src="https://www.dropbox.com/s/hjjabrdlol3clli/icon-people.png?raw=1" class="" alt="">
+                    <div class="mission-card">
+                      <h3>We encourage</h3>
+                      <p>We have a Google Group set up to share tools and documents</p>
+                    </div>
+                  </div>
+                </div>
+                <hr>
+      <!-- Mission Section End -->
+
+
+      <!-- Members Box -->
+            <div class="row members-row">
+              <h2>Our Members</h2>
+              <div class="col span-1-of-5">
+                <img src="https://www.dropbox.com/s/ykab8m4tnrqbx8o/cancer-research.png?raw=1" class="partners-img1"  alt="">
+              </div>
+              <div class="col span-1-of-5">
+                <img src="https://www.dropbox.com/s/k45az5vmfhsrtyo/compucorp.png?raw=1" class="partners-img2"  alt="">
+              </div>
+              <div class="col span-1-of-5">
+                <img src="https://www.dropbox.com/s/pi2lk27xcu8q4hw/kop.png?raw=1" class="partners-img3"  alt="">
+              </div>
+              <div class="col span-1-of-5">
+                <img src="https://www.dropbox.com/s/bmuiok7bkrycurr/zing.png?raw=1" class="partners-img4"  alt="">
+              </div>
+              <div class="col span-1-of-5">
+                <img src="https://www.dropbox.com/s/x75qbqpw4ttzbdr/comic-relief.png?raw=1" class="partners-img5"  alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Members Box End -->
+
+
+      <!-- Blog Section -->
+       <section class="blog-box ">
+        <div class="comtainer">
+          <div class="row blog-row">
+            <h2>Blog</h2>
+            <div class="col span-1-of-4">
+               <h5>Online Donations Special...</h5>
+               <hr class="blog-hr">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, voluptate, similique quas sunt aut recusandae in illo quia ut alias iste earum officia. </p>
+
+             <hr class="blog-hr-bottom">
+             <div class="date">
+                <p>14 Nov 2014</p>
+              </div>
+            </div>
+            <div class="col span-1-of-4">
+              <h5>Online Donations Special...</h5>
+               <hr class="blog-hr">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, voluptate, similique quas sunt aut recusandae in illo quia ut alias iste earum officia. </p>
+
+              <hr class="blog-hr-bottom">
+              <div class="date">
+                <p>14 Nov 2014</p>
+              </div>
+            </div>
+            <div class="col span-1-of-4">
+              <h5>Online Donations Special...</h5>
+               <hr class="blog-hr">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, voluptate, similique quas sunt aut recusandae in illo quia ut alias iste earum officia. </p>
+
+              <hr class="blog-hr-bottom">
+              <div class="date">
+                <p>14 Nov 2014</p>
+              </div>
+            </div>
+            <div class="col span-1-of-4">
+              <h5>Online Donations Special...</h5>
+               <hr class="blog-hr">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, voluptate, similique quas sunt aut recusandae in illo quia ut alias iste earum officia. </p>
+
+              <hr class="blog-hr-bottom">
+              <div class="date">
+                <p>14 Nov 2014</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+       <!-- Blog Section End -->
+
+
+      <!-- Footer Section -->
+      <section class="footer-section">
+        <div class="footer-col container">
+            <footer>
+              <div class="footer-top">
+                <div class="social-icons">
+                  <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                  <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                  <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                </div>
+                
+              </div>
+              <hr>
+              <div class="footer-bottom">
+                <p class="siteBy">This site was built by <a href="#/">Egomson Emmanuel</a></p>
+              </div>
+            </footer>
+        </div>
+      </section>
+      <!-- Footer Section End -->
+
+      
+    
+  </body>
+</html>
